ea822ef2cb650b985cbec31d0b73dbc0cff99db2
DenisuDevForFood <denisfeier98@yahoo.com>
2021-04-24 16:07:16 +0300

    Initial Commit

---
+++ mReady/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mReady
+//
+//  Created by Denis Feier on 24.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
